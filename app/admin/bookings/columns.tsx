3fe5fba9799649d5310b4901e58c645e75a3121f
shankar <shankar.trs02@gmail.com>
2025-07-08 11:14:27 +0530

    refactor: Clean up BookingForm and related pages by removing unnecessary fragments and adding fetchServices call

--- app/admin/bookings/columns.tsx
+++ app/admin/bookings/columns.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from "react"
 import { Button } from "components/ui/button"
 import { useOfferStore } from "stores/-offerStore"
 import { Edit, SendHorizontal, Trash, Eye, ChevronDown } from "lucide-react"
