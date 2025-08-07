3cec0c5e83eb33ba4a146a24a7d2c59a00c8e1fd
shankar <shankar.trs02@gmail.com>
2025-08-07 15:35:19 +0530

    feat!(notification) : set the notification sound in socket message

--- lib/capitalize.ts
+++ lib/capitalize.ts
@@ -1,3 +1,10 @@
 export const capitalize = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const playNotificationSound = () => {
+    const audio = new Audio('/audio/notification.mp3');
+    audio.play().catch((err) => {
+      console.warn('Failed to play notification sound $>>', err);
+    });
+  };
