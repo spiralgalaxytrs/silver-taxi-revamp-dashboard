57807db5cbbbc24d9b429ab65a88c489b0e01708
shankar1906 <shankar.trs02@gmail.com>
2025-11-11 23:02:03 +0530

    image change tp unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'silvertaxi.blr1.digitaloceanspaces.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'silver-taxi-images.blr1.digitaloceanspaces.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.silvertaxi.in',
-        pathname: '/image/v1/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        pathname: '/image/v1/**',
-      },
-    ],
+   unoptimized: true
   },
   eslint: {
     ignoreDuringBuilds: true,
